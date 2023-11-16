c4f8c0302e015cf637a4321178c4525dd6f40d89
Alex Zhukov <a@jukov.info>
2023-11-16 19:11:52 +0100

    Add CollectionFragment. Update SDK to 34.

--- app/src/main/java/info/jukov/rijksmuseum/MainActivity.kt
+++ app/src/main/java/info/jukov/rijksmuseum/MainActivity.kt
@@ -1,46 +1,20 @@
 package info.jukov.rijksmuseum
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import info.jukov.rijksmuseum.ui.theme.RijksmuseumTheme
+import androidx.fragment.app.FragmentActivity
+import info.jukov.rijksmuseum.ui.feature.list.presentation.CollectionFragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RijksmuseumTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+        savedInstanceState ?: showCollectionFragment()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RijksmuseumTheme {
-        Greeting("Android")
+    private fun showCollectionFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(android.R.id.content, CollectionFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/info/jukov/rijksmuseum/ui/feature/list/presentation/CollectionFragment.kt
@@ -0,0 +1,55 @@
+package info.jukov.rijksmuseum.ui.feature.list.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import info.jukov.rijksmuseum.ui.theme.RijksmuseumTheme
+
+class CollectionFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                RijksmuseumTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Greeting("Android")
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        RijksmuseumTheme {
+            Greeting("Android")
+        }
+    }
+}
