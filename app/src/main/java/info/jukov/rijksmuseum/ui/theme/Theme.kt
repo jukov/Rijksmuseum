772a76f309781c373da7047b562c4732e631f274
Alex Zhukov <a@jukov.info>
2023-11-21 21:19:33 +0100

    Light theme fixes.

--- app/src/main/java/info/jukov/rijksmuseum/ui/theme/Theme.kt
+++ app/src/main/java/info/jukov/rijksmuseum/ui/theme/Theme.kt
@@ -1,17 +1,13 @@
 package info.jukov.rijksmuseum.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -82,27 +78,25 @@ private val darkColorScheme = darkColorScheme(
 @Composable
 fun RijksmuseumTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,//TODO is it possible to combine dyn theme and app color?
     content: @Composable() () -> Unit
 ) {
-    //TODO light theme fixes
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> darkColorScheme
-        else -> lightColorScheme
+    val colorScheme = if (darkTheme) {
+        darkColorScheme
+    } else {
+        lightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.onPrimary.toArgb()
+            window.statusBarColor = if (darkTheme) {
+                colorScheme.onPrimary.toArgb()
+            } else {
+                colorScheme.secondary.toArgb()
+            }
 
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
